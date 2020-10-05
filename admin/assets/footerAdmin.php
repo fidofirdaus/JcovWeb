0f8f938e583c6155805f9eec5196307675f0cf04
Muhammad Firdaus <firdauspriyono17@gmail.com>
2020-10-05 12:06:32 +0700

    Add files via upload

---
+++ admin/assets/footerAdmin.php
@@ -0,0 +1,34 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+
+
+
+    <div class="container py-4">
+        <div class="copyright">
+            &copy; Copyright <strong><span>Pemrograman Website A</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+        </div>
+    </div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/venobox/venobox.min.js"></script>
+<script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+<script src="assets/js/pesan.js"></script>
+
+</body>
+
+</html>
